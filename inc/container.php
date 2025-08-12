f9b392478d73b72e5d50f91e1d2dd9192bdb19e7
MarufAhamed-ui <0323653944@student.wub.edu.bd>
2025-08-12 22:16:24 +0600

    Add files via upload

---
+++ inc/container.php
@@ -0,0 +1,13 @@
+</head>
+<body class="">
+<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
+  <div class="container d-flex justify-content-center">
+    <a href="http://localhost/sfos" 
+       class="navbar-brand text-center fw-bold brand-gradient"
+       style="font-family: 'Poppins', sans-serif; letter-spacing: 2px;">
+       Savory Station
+    </a>
+  </div>
+</nav>
+
+
